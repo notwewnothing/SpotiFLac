d5381afcf9f50ee8bbd8c176f9680749a8fd0d04
zarzet <zarzet@users.noreply.github.com>
2026-03-14 21:49:22 +0700

    chore: bump app version to 3.8.0+106

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -3,8 +3,8 @@ import 'package:flutter/foundation.dart';
 /// App version and info constants
 /// Update version here only - all other files will reference this
 class AppInfo {
-  static const String version = '3.7.2';
-  static const String buildNumber = '105';
+  static const String version = '3.8.0';
+  static const String buildNumber = '106';
   static const String fullVersion = '$version+$buildNumber';
 
   /// Shows "Internal" in debug builds, actual version in release.
